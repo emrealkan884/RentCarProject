dddab1f09eb46f73dc6e52f1158c4f1ae3f0aa0f
emrealkan884 <Hasan.alkan884@gmail.com>
2021-02-28 20:06:55 +0300

    Stage5

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal : EfEntityRepositoryBase<Brand,RentACarContext>
+    public class EfBrandDal : EfEntityRepositoryBase<Brand,RentACarContext>,IBrandDal
     {
         
     }
--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal : EfEntityRepositoryBase<Color,RentACarContext>
+    public class EfColorDal : EfEntityRepositoryBase<Color,RentACarContext>,IColorDal
     {
        
     }
